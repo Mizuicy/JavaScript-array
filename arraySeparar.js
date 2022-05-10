122f68eb7c21c9ccb45f9eee5f8fa7ece13620be
Mizuicy <102737167+Mizuicy@users.noreply.github.com>
2022-05-10 09:52:14 -0300

    Add files via upload

---
+++ arraySeparar.js
@@ -0,0 +1,6 @@
+const nomes = ['Bluno', 'PaideTodos', 'Duda', 'Lary', 'Day', 'Miguelito', 'KauanUBranco', 'Oto'];
+
+let sala1 = nomes.slice(0, nomes.length/2)
+let sala2 = nomes.slice(nomes.length/2)
+console.log (sala1)
+console.log (sala2)
---
+++ arraySubstituir.js
@@ -0,0 +1,3 @@
+const nomes = ['Bluno', 'PaideTodos', 'Duda', 'Lary']
+nomes.splice(1,2,'Miles')
+console.log(nomes)
